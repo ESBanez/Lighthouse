df7f8a52671da4cd81553a366b3824afea2dde2b
Ederick <esb.tailor@gmail.com>
2024-04-14 22:52:39 +0800

    fix the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -130,19 +130,7 @@ function Footer() {
             </div>
             <br/>
             <div className="layout c-2">
-              <div className="layout_item w-50">
-                <ul className="LahatNgFlex LahatNgUl ">
-                  <li>
-                    {/* All rights reserved */}
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32">
-                      <path fill="none" d="M0 0h24v24H0z" />
-                      <path d="M12 6.654a6.786 6.786 0 0 1 2.596 5.344A6.786 6.786 0 0 1 12 17.34a6.786 6.786 0 0 1-2.596-5.343A6.786 6.786 0 0 1 12 6.654zm-.87-.582A7.783 7.783 0 0 0 8.4 12a7.783 7.783 0 0 0 2.728 5.926 6.798 6.798 0 1 1 .003-11.854zm1.742 11.854A7.783 7.783 0 0 0 15.6 12a7.783 7.783 0 0 0-2.73-5.928 6.798 6.798 0 1 1 .003 11.854z" />
-                    </svg> */}
-                  </li>
-                  {/* Other social media icons */}
-                </ul>
-              </div>
-              <div className="layout_item w-25">
+              <div className="layout_item">
                 <ul className="LahatNgFlex LahatNgUl">
                   <li>
                     <a href="#section5" className="footerlogoholder" ><img src="logo1.png"/></a>
