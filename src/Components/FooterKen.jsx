604bedb61166a3862c67a54f29e670b0c12fbe34
Ederick S. Banez <esb.tailor@gmail.com>
2024-05-14 21:49:50 +0800

    change the footer bg

--- src/Components/FooterKen.jsx
+++ src/Components/FooterKen.jsx
@@ -168,6 +168,7 @@ function FooterKen () {
                 height="150"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.1113764058726!2d120.98279201446428!3d14.562442280006851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c8e599ca44df%3A0x3b77c764b7c6da6e!2s2100%20Madre%20Ignacia%20St%2C%20Malate%2C%20Manila%2C%201004%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1642095819724!5m2!1sen!2sph"
                 allowFullScreen
+                target="_blank"
                     />
 
                     <CardContent>
