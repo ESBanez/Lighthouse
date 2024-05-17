44041b7b1edf793f49bef600de106a413485e702
Ederick <esb.tailor@gmail.com>
2024-05-17 08:53:39 +0800

    fix the footer in email sending to me and the autoreply

--- src/Components/FooterKen.jsx
+++ src/Components/FooterKen.jsx
@@ -1,4 +1,3 @@
-// Import necessary components
 import { useState } from 'react';
 import Button from "@mui/material/Button";
 import Card from '@mui/material/Card';
@@ -9,13 +8,12 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import FormHelperText from '@mui/material/FormHelperText';
 import emailjs from 'emailjs-com';
-import "../Sass/footerken.css"
+import "../Sass/footerken.css";
 
 // Initialize emailjs
-emailjs.init('-DhGdulE7p83TuON2')
+emailjs.init('-DhGdulE7p83TuON2');
 
-function FooterKen () {
-    // State variables for form inputs
+function FooterKen() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
@@ -23,7 +21,6 @@ function FooterKen () {
     const [inquiry, setInquiry] = useState('');
     const [message, setMessage] = useState('');
 
-    // Event handlers for form inputs
     const handleNameChange = (e) => {
         setName(e.target.value);
     };
@@ -44,37 +41,53 @@ function FooterKen () {
         setInquiry(e.target.value);
     };
 
-
     const handleMessageChange = (e) => {
         setMessage(e.target.value);
     };
 
-    // Handle form submission
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         const btn = document.getElementById('button');
-        btn.value = 'Sending...';
+        btn.textContent = 'Sending...';
 
         try {
-            await emailjs.sendForm('service_kq880qc', 'template_j67t39c', e.target, '-DhGdulE7p83TuON2');
+            const formData = {
+                name,
+                email,
+                phone,
+                company,
+                inquiry,
+                message,
+            };
+            await emailjs.send('service_kq880qc', 'template_j67t39c', formData, '-DhGdulE7p83TuON2');
             alert('Email sent successfully!');
-            (true);
+
+
+            // Reset form fields
+            setName('');
+            setEmail('');
+            setPhone('');
+            setCompany('');
+            setInquiry('');
+            setMessage('');
+
+            btn.textContent = 'Submit';
         } catch (error) {
             console.error('Error sending email:', error);
             alert('Error sending email. Please try again later.');
+            btn.textContent = 'Submit';
         }
     };
 
     return (
         <div className="footercomponent">
             <video autoPlay loop muted className="video-background">
-                <source src="backvid.mp4" type="video/mp4"/>
+                <source src="backvid.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className="footerbody container">
                 <form className="footerform" onSubmit={handleSubmit}>
-                    {/* Name and Email fields */}
                     <div className="footernameemail">
                         <div className="px-3">
                             <TextField
@@ -99,7 +112,6 @@ function FooterKen () {
                             </FormHelperText>
                         </div>
                     </div>
-                    {/* Phone and Company fields */}
                     <div className="footerphonecompany">
                         <div className="px-3">
                             <TextField
@@ -119,27 +131,21 @@ function FooterKen () {
                             />
                         </div>
                     </div>
-                                                
-
-                    <br/>
-                    {/* Inquiry field */}
+                    <br />
                     <div className="px-3">
                         <TextField
-                        id="message"
-                        label="Inquiries"
-                        multiline
-                        style={{ width: "100%" }}
-                        value={inquiry}
-                        onChange={handleInquiryChange}
-                        required
+                            id="inquiry"
+                            label="Inquiries"
+                            multiline
+                            style={{ width: "100%" }}
+                            value={inquiry}
+                            onChange={handleInquiryChange}
+                            required
                         />
                     </div>
-                                                
-
-                    <br/>
+                    <br />
                     <div className="footerinquiries">
                         <div className="footerinquiriestab px-3">
-
                             <TextField
                                 id="message"
                                 label="Message"
@@ -152,95 +158,85 @@ function FooterKen () {
                             />
                         </div>
                     </div>
-                    {/* Submit button */}
                     <div className="footersubmit">
                         <Button variant="contained" id="button" type="submit">Submit</Button>
                     </div>
                 </form>
-        <div className="footeremail">
-        {/* World map background */}
-            <div className="footeremailcardtab">
-                <Card sx={{ 
-                    maxWidth: 340,  
-                    width:"15rem"   ,
-                    boxShadow: "5px 5px 25px 0px rgba(46, 61, 73, 0.2)",
-                    borderRadius: "10px",
-                }}>
-                    <CardMedia
-                component="iframe"
-                title="Altoha Building Location"
-                height="150"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.1113764058726!2d120.98279201446428!3d14.562442280006851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c8e599ca44df%3A0x3b77c764b7c6da6e!2s2100%20Madre%20Ignacia%20St%2C%20Malate%2C%20Manila%2C%201004%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1642095819724!5m2!1sen!2sph"
-                allowFullScreen
-                target="_blank"
-                    />
-
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                        Malate, Manila
-                        </Typography>
-                    </CardContent>
-
-                    <CardActions style={{    display: "flex", justifyContent: "space-around"}}>
-                        <Button size="small">Share</Button>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                </Card>
-            </div>
-            <br/>
-            <br/>
-            <br/>
-            <div className="footeremailtab">
-                <div className="footeremailustitle">
-                    Email Us
+                <div className="footeremail">
+                    <div className="footeremailcardtab">
+                        <Card sx={{ 
+                            maxWidth: 340,  
+                            width: "15rem",
+                            boxShadow: "5px 5px 25px 0px rgba(46, 61, 73, 0.2)",
+                            borderRadius: "10px",
+                        }}>
+                            <CardMedia
+                                component="iframe"
+                                title="Altoha Building Location"
+                                height="150"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.1113764058726!2d120.98279201446428!3d14.562442280006851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c8e599ca44df%3A0x3b77c764b7c6da6e!2s2100%20Madre%20Ignacia%20St%2C%20Malate%2C%20Manila%2C%201004%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1642095819724!5m2!1sen!2sph"
+                                allowFullScreen
+                                target="_blank"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Malate, Manila
+                                </Typography>
+                            </CardContent>
+                            <CardActions style={{ display: "flex", justifyContent: "space-around" }}>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <div className="footeremailtab">
+                        <div className="footeremailustitle">
+                            Email Us
+                        </div>
+                        <div className="footeremaillist">
+                            <li className="c-nav-tool_item">
+                                <a href="#section2" className="emails">
+                                    info@lighthousemanila.com
+                                </a>
+                            </li>
+                            <li className="c-nav-tool_item">
+                                <a href="#section5" className="emails">
+                                    support@btsolve.com
+                                </a>
+                            </li>
+                            <li className="c-nav-tool_item">
+                                <a href="#section3" className="emails">
+                                    info@rapidonline.ph
+                                </a>
+                            </li>
+                        </div>
+                    </div>
                 </div>
-                <div className="footeremaillist">
-                        <li className="c-nav-tool_item">
-                        <a href="#section2" className="emails">
-                            info@lighthousemanila.com
-                        </a>
-                        </li>
-                        <li className="c-nav-tool_item">
-                        <a href="#section5" className="emails">
-                            support@btsolve.com
-                        </a>
-                        </li>
-                        <li className="c-nav-tool_item">
-                        <a href="#section3" className="emails">
-                            info@rapidonline.ph
-                        </a>
-                        </li>
+            </div>
+            <br />
+            <div className="footerlogo container">
+                <div className="footerlogotab">
+                    <a href="#section5" className="footerlogoholder">
+                        <img src="logo btsolve black.png" alt="BTSolve Logo"/>
+                    </a>
+                    <a href="#section3" className="footerlogoholder">
+                        <img src="logo rapid.png" alt="Rapid Logo"/>
+                    </a>
+                    <a href="#section2" className="footerlogoholder">
+                        <img src="logo lighthouse.png" alt="Lighthouse Logo"/>
+                    </a>
+                </div>
+            </div>
+            <div className="footerall">
+                <div className="footeralltab">
+                    All rights reserved 2024.
                 </div>
             </div>
         </div>
-    </div>
-    <br/>
-    <div className="footerlogo container"   
-        // data-aos="fade-up-right"
-        >
-        <div className="footerlogotab">
-            <a href="#section5" className="footerlogoholder">
-                <img src="logo btsolve black.png"/>
-            </a>
-
-            <a href="#section3" className="footerlogoholder">
-                <img src="logo rapid.png"/>
-            </a>
-
-            <a href="#section2" className="footerlogoholder">
-                <img src="logo lighthouse.png"/>
-            </a>
-
-        </div>
-    </div>
-    <div className="footerall">
-        <div className="footeralltab">
-            All rights reserved 2024.
-        </div>
-    </div>
-  </div>
-);
+    );
 }
-
 
 export default FooterKen;
