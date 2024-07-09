23fd1b90803e66681ea051527b04607402f400d2
Ederick <esb.tailor@gmail.com>
2024-07-09 21:05:19 +0800

    fix the cache in the index.html using '/src/Javascript/serviceWorker.js'

--- src/Javascript/serviceWorker.js
+++ src/Javascript/serviceWorker.js
@@ -1,44 +1,48 @@
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
   '/',
-  '../App.jsx',
-  '../Main.jsx',
-  '../Pages/Home.jsx',
+  '/App.jsx',                // Adjust path if needed
+  '/Main.jsx',
+  '/Pages/Home.jsx',
 
-  '../Components/Btsolve2.jsx',
-  '../Components/Divider.jsx',
-  '../Components/FooterKen.jsx',
-  '../Components/LighthousePart.jsx',
-  '../Components/MissionVision.jsx',
-  '../Components/Navbar.jsx',
-  '../Components/ProductCarousel.jsx',
-  '../Components/Rapid2.jsx',
-  '../Components/RapidCarousel.jsx',
-  '../Components/Testimonials.jsx',
+  '/Components/Btsolve2.jsx',
+  '/Components/Divider.jsx',
+  '/Components/FooterKen.jsx',
+  '/Components/LighthousePart.jsx',
+  '/Components/MissionVision.jsx',
+  '/Components/Navbar.jsx',
+  '/Components/ProductCarousel.jsx',
+  '/Components/Rapid2.jsx',
+  '/Components/RapidCarousel.jsx',
+  '/Components/Testimonials.jsx',
 
-  '../Sass/Bts.css',
-  '../Sass/footerken.css',
-  '../Sass/Headline.css',
-  '../Sass/Home.css',
-  '../Sass/LigthousePart.css',
-  '../Sass/maincarousel.css',
-  '../Sass/missionvision.css',
-  '../Sass/mydivider.css',
-  '../Sass/Navbar.css',
-  '../Sass/productcarousel.scss',
-  '../Sass/Rapid1.css',
-  '../Sass/rapidcarousel.scss',
-  '../Sass/Services.scss',
-  '../Sass/Rapid.css',
-  '../Sass/testimonials.css',
-  
-  // Add other static assets to cache
+  '/Sass/Bts.css',
+  '/Sass/footerken.css',
+  '/Sass/Headline.css',
+  '/Sass/Home.css',
+  '/Sass/LigthousePart.css',
+  '/Sass/maincarousel.css',
+  '/Sass/missionvision.css',
+  '/Sass/mydivider.css',
+  '/Sass/Navbar.css',
+  '/Sass/productcarousel.scss',
+  '/Sass/Rapid1.css',
+  '/Sass/rapidcarousel.scss',
+  '/Sass/Services.scss',
+  '/Sass/Rapid.css',
+  '/Sass/testimonials.css',
+
+  '/public/assets/lighthousewebvid.webm',
+  '/public/assets/backvid.mp4',
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
